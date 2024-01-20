81bc69577f3439c1ac8f2f180f764d19c42f8645
mwangiowen <owen31588@gmail.com>
2024-01-20 21:32:10 +0300

    Done

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import ipdb
+import os  # Import the os module for checking the current directory
 
 # projects: kickstarter.select("li.project.grid_4")[0]
 # title: project.select("h2.bbcard_name strong a")[0].text
@@ -8,24 +9,33 @@ import ipdb
 # location: project.select("ul.project-meta span.location-name")[0].text
 # percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
 
+
 def create_project_dict():
-    html = ''
-    with open('./fixtures/kickstarter.html') as file:
+    # Print the current working directory for debugging
+    print("Current Directory:", os.getcwd())
+
+    html = ""
+    with open("../fixtures/kickstarter.html") as file:
         html = file.read()
-    kickstarter = BeautifulSoup(html, 'html.parser')
+    kickstarter = BeautifulSoup(html, "html.parser")
     projects = {}
     # Iterate through the projects
     for project in kickstarter.select("li.project.grid_4"):
         title = project.select("h2.bbcard_name strong a")[0].text
         projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
+            "image_link": project.select("div.project-thumbnail a img")[0]["src"],
+            "description": project.select("p.bbcard_blurb")[0].text,
+            "location": project.select("ul.project-meta span.location-name")[0].text,
+            "percent_funded": project.select("ul.project-stats li.first.funded strong")[
+                0
+            ].text.replace("%", ""),
         }
     # return the projects dictionary
-
     return projects
 
-projects = create_project_dict()
-print(projects)
+
+# Uncomment the following line to check the current working directory
+# print("Current Directory:", os.getcwd())
+
+projects_data = create_project_dict()
+print(projects_data)
